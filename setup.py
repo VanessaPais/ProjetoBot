633fc81a354b889dc2b94d53c7f7be5d5ef848ba
MINGW64 <vanes@DESKTOP-T1IVMKO>
2022-07-19 22:39:42 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bot do telegram de analise de dados',
+    author='Vanessa Pais',
+    license='',
+)
